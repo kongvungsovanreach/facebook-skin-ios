52d55f7d82f9ac1907eb3af7c938c8e8dfe2e45b
Kong vungsovanreach <vungsovanreachkong@gmail.com>
2018-12-02 20:39:07 +0700

    update

--- FacebookHomework/customcell/TextPostCell.swift
+++ FacebookHomework/customcell/TextPostCell.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class TextPostCell: UITableViewCell {
-
+    @IBOutlet weak var profilePicture: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        profilePicture.layer.cornerRadius = profilePicture.frame.height / 2
+        profilePicture.layer.masksToBounds = true
+
+        
+//        let mScreenSize = UIScreen.main.bounds
+//        let mSeparatorHeight = CGFloat(9.0) // Change height of speatator as you want
+//        let mAddSeparator = UIView.init(frame: CGRect(x: 0, y: self.frame.size.height - mSeparatorHeight, width: mScreenSize.width, height: mSeparatorHeight))
+//        mAddSeparator.backgroundColor = UIColor(displayP3Red: 192/255, green: 192/255, blue: 192/255, alpha: 1) // Change backgroundColor of separator
+//        self.addSubview(mAddSeparator)
         // Initialization code
     }
 
--- FacebookHomework/viewcontroller/ViewController.swift
+++ FacebookHomework/viewcontroller/ViewController.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 class ViewController: UIViewController {
-
+    @IBOutlet weak var postTableView: UITableView!
+    
     @IBOutlet weak var bottomTabbar: UITabBar!
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bottomTabbar.delegate = self
-        
-//        navigatorConfig()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    func navigatorConfig() {
-        navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 50/255, green: 109/255, blue: 252/255, alpha: 1)
-        navigationController?.navigationBar.tintColor = .red
-//            UIColor(red: 59/255, green: 89/255, blue: 152/255, alpha: 1)
+//        postTableView.separatorStyle = 
+        postTableView.dataSource = self
+        postTableView.delegate = self
+        postTableView.register(UINib(nibName: "TextPostCell", bundle: nil), forCellReuseIdentifier: "TextPostReusableCell")
+//
+        postTableView.rowHeight = UITableView.automaticDimension
+        postTableView.estimatedRowHeight = UITableView.automaticDimension
     }
 
 
 }
 
 
-extension ViewController : UITabBarDelegate {
-    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-//        print(item.)
+
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
     }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reusableCell = tableView.dequeueReusableCell(withIdentifier: "TextPostReusableCell", for: indexPath) as! TextPostCell
+        return reusableCell
+    }
+}
+extension ViewController : UITableViewDelegate {
+
 }
